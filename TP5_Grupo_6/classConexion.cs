8dc6647dd7f63124d1e059a68247210e1df8e938
GabrielErizaga <gabriel.erizaga@alumnos.frgp.utn.edu.ar>
2025-04-30 16:46:37 -0300

    tp5 ejercicio 1

--- TP5_Grupo_6/classConexion.cs
+++ TP5_Grupo_6/classConexion.cs
@@ -9,6 +9,10 @@ namespace TP5_Grupo_6
 	public class classConexion
 	{
 		private const string cadenaconexion =  "Data Source=localhost\\sqlexpress; Initial Catalog=BDSucursales;Integrated Security = True";
-
+		int filaAfectada;
+		public int SucursalAltaBaja (string consultaSQL)
+		{
+			return filaAfectada;
+		}
     }
 }
--- TP5_Grupo_6/tp05EjercicioA.aspx.cs
+++ TP5_Grupo_6/tp05EjercicioA.aspx.cs
@@ -11,14 +11,17 @@ namespace TP5_Grupo_6
 	public partial class tp05EjercicioA : System.Web.UI.Page
 	{
 		string consultaSQL;
+		int filaAfectada;
 		classConexion conexion = new classConexion();
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+		
 		}
 
         protected void buttonAceptar_Click(object sender, EventArgs e)
         {
+			consultaSQL = "INSERT Sucursal (NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal) VALUES ('" + textboxNombre.Text + "', '" + textboxDescripcion.Text + "'," + ddlProvincia.SelectedValue + ",'" + textboxDireccion.Text + "')";
+			filaAfectada = conexion.SucursalAltaBaja(consultaSQL);
 
         }
     }
