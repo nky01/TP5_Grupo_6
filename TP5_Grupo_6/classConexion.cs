521eedcbfe321a1433701307800599d26a2bf459
GabrielErizaga <gabriel.erizaga@alumnos.frgp.utn.edu.ar>
2025-04-30 16:36:44 -0300

    tp5 ejercicio 1

---
+++ TP5_Grupo_6/classConexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace TP5_Grupo_6
+{
+	public class classConexion
+	{
+		private const string cadenaconexion =  "Data Source=localhost\\sqlexpress; Initial Catalog=BDSucursales;Integrated Security = True";
+
+    }
+}
---
+++ TP5_Grupo_6/tp05EjercicioA.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace TP5_Grupo_6
+{
+	public partial class tp05EjercicioA : System.Web.UI.Page
+	{
+		string consultaSQL;
+		classConexion conexion = new classConexion();
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void buttonAceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
