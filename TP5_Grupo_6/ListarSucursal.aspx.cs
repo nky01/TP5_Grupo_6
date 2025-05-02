cd8ea32d8f43ab02c834a50d26048e3a8bd4a369
Lun <ludesantosherrero@gmail.com>
2025-05-02 19:40:05 -0300

    Configuración Inicial de GridView

--- TP5_Grupo_6/ListarSucursal.aspx.cs
+++ TP5_Grupo_6/ListarSucursal.aspx.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TP5_Grupo_6
 {
     public partial class ListadoSucursales : System.Web.UI.Page
     {
+        string consultasql;
+        private Conexion conexion;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                consultasql = "SELECT Id_Sucursal, NombreSucursal, DescripcionSucursal, DescripcionProvincia, DireccionSucursal from Sucursal inner join Provincia on Id_ProvinciaSucursal = Id_Provincia";
 
+                conexion = new Conexion();
+                conexion.Conectar();
+
+                SqlDataReader reader = conexion.EjecutarConsulta(consultasql);
+                if (reader != null)
+                {
+                    DataTable tabla = new DataTable();
+                    tabla.Load(reader);
+                    reader.Close();
+                    conexion.Cerrar();
+
+                    gvSucursal.DataSource = tabla;
+                    gvSucursal.DataBind();
+                }
+
+                conexion.Cerrar();
+            }
         }
     }
 }
